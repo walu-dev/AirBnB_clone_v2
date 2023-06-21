b54e8c5d49ac1b2db60d9b2aeb6cfbdcd2f6461e
walu-dev <jonathanenoch11@gmail.com>
2023-06-21 23:57:57 +0000

    fin

--- models/review.py
+++ models/review.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""Module for Review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
-from models.base_model import BaseModel
 
-
-class Review(BaseModel):
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class Review(BaseModel, Base):
+    """This is for the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
